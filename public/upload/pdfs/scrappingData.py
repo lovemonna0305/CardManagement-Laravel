fd65f8c0b58f72e95c1e3ee74809adb183297bff
TopDeveloper <121497691+TopDeveloper215@users.noreply.github.com>
2024-07-05 05:37:54 +0900

    add pdf scrapping feature

--- public/upload/pdfs/scrappingData.py
+++ public/upload/pdfs/scrappingData.py
@@ -1,29 +1,42 @@
 import pdfplumber
 
+pdf_file_path = '6850003398669 VT EDNEUSA.pdf'
+extracted_data =[]
 # Open the PDF file
-with pdfplumber.open('forpdf.pdf') as pdf:
+with pdfplumber.open(pdf_file_path) as pdf:
     # Get the number of pages in the PDF
     num_pages = len(pdf.pages)
-
-    data_array = []
-
     # Loop through each page and extract the text
     for page_num in range(num_pages):
         page = pdf.pages[page_num]
-        data = page.extract_text()
-        if page_num == 1:
-            for line in data.strip().split("\n"):
-                if "Data Hora Uso Carga Tarifa Tipo de Tarifa Operadora Nº Série CCIT Linha Valor Utiliz. (UT)" in line:
-                    continue
-                if line.strip():
-                    parts = line.split()
-                    data_array.append(" ".join(parts))
+        text = page.extract_text()
+        lines = text.split('\n')
+        for line in lines:
+            columns = line.strip().split()
+            if 'Cartão' in columns and 'VALE' in columns:
+                cartao_index = columns.index('Cartão')
+                if cartao_index + 1 < len(columns):
+                    card_number = columns[cartao_index + 1]
+                    # print('card_number', card_number)
+            if len(columns) == 10 and columns[1].count(':') == 2 and columns[5] == 'VT':
+                row_data = {
+                    "Working_Days": columns[0],
+                    "Working_Hour": columns[1],
+                    # "Carga": columns[2],
+                    # "Tarifa": columns[3],
+                    # "Tipo": columns[4],
+                    # "de Tarifa": columns[5],
+                    # "Operadora": columns[6],
+                    # "Nº Série CCIT": columns[7],
+                    "Bus_Line": columns[8],
+                    # "Valor Utiliz.": columns[9]
+                }
+                extracted_data.append(row_data)
 
-                for row in data_array:
-                    print(row)
-        
-        # print(f'Page {page_num + 1}: {text}')
+final_output = {
+    "card_number": card_number,
+    "datas": extracted_data
+}
+print('final_output', final_output)
 
-        
-
-        
+                
