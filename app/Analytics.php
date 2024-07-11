d7f985898255506b83a91cdadf92bc380b9dcde0
TopDeveloper <121497691+TopDeveloper215@users.noreply.github.com>
2024-07-11 22:23:41 +0900

    remove unnecessary parts

--- app/Analytics.php
+++ app/Analytics.php
@@ -8,7 +8,7 @@ class Analytics extends Model {
 
 	protected $table = 'analytics';
 
-	protected $fillable = ['card_id', 'non_working_days', 'non_working_hours', 'non_bus_lines','file_url'];
+	protected $fillable = ['card_id', 'non_working_days', 'non_working_hours', 'non_bus_lines','file_url','nwd_content', 'nwh_content', 'nbl_content'];
 
 	protected $hidden = ['created_at', 'updated_at'];
 
--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -156,14 +156,14 @@ class AnalyticsController extends Controller {
 				'non_working_days' => $analytic->non_working_days,
 				'non_working_hours' => $analytic->non_working_hours,
 				'non_bus_lines' => $analytic->non_bus_lines,
-				'file_url' => $analytic->file_url,
+				'file_url' => str_replace('./upload/pdfs/', '', $analytic->file_url),
 			];
 		}
 		return Datatables::of($datas)
 			->addColumn('action', function ($datas) {
 				return '<a href="exportAnalyticsPDF/' . $datas->id . ')" class="btn btn-success btn-xs"><i class="fa fa-file-pdf-o"></i> Export PDF</a> ' .
-				'<a onclick="exportExcel(' . $datas->id . ')" class="btn btn-success btn-xs"><i class="fa fa-file-excel-o"></i> Export Excel</a> ' .
-				'<a onclick="editForm(' . $datas->id . ')" class="btn btn-success btn-xs"><i class="fa fa-file-excel-o"></i> Edit</a> ' .
+				// '<a onclick="exportExcel(' . $datas->id . ')" class="btn btn-success btn-xs"><i class="fa fa-file-excel-o"></i> Export Excel</a> ' .
+				// '<a onclick="editForm(' . $datas->id . ')" class="btn btn-success btn-xs"><i class="fa fa-file-excel-o"></i> Edit</a> ' .
 				'<a onclick="deleteData(' . $datas->id . ')" class="btn btn-danger btn-xs"><i class="glyphicon glyphicon-trash"></i> Delete</a>';
 			})
 			->rawColumns(['action'])->make(true);
@@ -263,18 +263,26 @@ class AnalyticsController extends Controller {
 			$x=0; 
 			$y=0; 
 			$z=0; 
+
+			$non_working_days = [];
+			$non_working_hours = [];
+			$non_bus_lines = [];
 			
 			$working_days =  explode(",", $card->working_days);
 			
+			
 			$hours = explode(",", $card->usage_hours);
+			// dd('$hours', $hours);
 			$bus_lines = $card->bus_lines;
 
 			$usage_hours = [];
+			
 			foreach ($extract_datas as $data){
 				if (in_array($data['working_day'], $working_days)) {
 					// echo "The date " . $data->working_day . " is present in the array.\n". PHP_EOL;
 				} else {
 					$x++;
+					$non_working_days [] = $data;
 
 					// X Variable
 					// echo "The date " . $data->working_day . " is not present in the array.\n". PHP_EOL;      
@@ -313,6 +321,7 @@ class AnalyticsController extends Controller {
 				if (!$is_time_found) {
 					// Y Variable
 					$y++;
+					$non_working_hours [] = $data;
 					// echo "The time $time is not within any of the ranges in the \$usage_hours array.\n";
 				}
 				$compareBusLines = $data['bus_line'];
@@ -322,88 +331,11 @@ class AnalyticsController extends Controller {
 					// Z Variable
 					// echo "The Bus lines not contains! \n";
 					$z++;
+					$non_bus_lines [] = $data;
 				}
 			}
 
 
-
-
-			// $cards = Card::all();
-
-
-			// $x=0; 
-			// $y=0; 
-			// $z=0; 
-			
-			// foreach ($cards as $card){
-			// 	$working_days =  explode(",", $card->working_days);
-			// 	$hours = explode(",", $card->usage_hours);
-			// 	$bus_lines = $card->bus_lines;
-
-			// 	$usage_hours = [];
-
-			// 	foreach ($outputArray->datas as $data){
-	
-			// 		if (in_array($data->working_day, $working_days)) {
-			// 			// echo "The date " . $data->working_day . " is present in the array.\n". PHP_EOL;
-			// 		} else {
-			// 			$x++;
-	
-			// 			// X Variable
-			// 			// echo "The date " . $data->working_day . " is not present in the array.\n". PHP_EOL;      
-			// 		}
-	
-				
-				
-			// 	foreach ($hours as $key => $value) {
-			// 		if (array_key_exists($value, $real_hours)) {
-			// 			$usage_hours [] = $real_hours[$value];
-			// 		}
-			// 	}
-				
-			// 	// $time = "10:54:31";
-			// 	$time = $data->working_hour;
-
-				
-			// 	$is_time_found = false;
-			// 	foreach ($usage_hours as $hour_range) {
-			// 		$start_time = explode("-", $hour_range)[0];
-			// 		$end_time = explode("-", $hour_range)[1];
-				
-			// 		$start_time_parts = explode(":", $start_time);
-			// 		$end_time_parts = explode(":", $end_time);
-				
-			// 		$start_time_seconds = ($start_time_parts[0] * 3600) + ($start_time_parts[1] * 60);
-			// 		$end_time_seconds = ($end_time_parts[0] * 3600) + ($end_time_parts[1] * 60);
-				
-			// 		$time_parts = explode(":", $time);
-			// 		$time_seconds = ($time_parts[0] * 3600) + ($time_parts[1] * 60) + $time_parts[2];
-				
-			// 		if ($time_seconds >= $start_time_seconds && $time_seconds <= $end_time_seconds) {
-			// 			$is_time_found = true;
-			// 			// echo "The time $time is within the range " . $hour_range . ".\n";
-			// 			break;
-			// 		}
-			// 	}
-				
-			// 	if (!$is_time_found) {
-			// 		// Y Variable
-			// 		$y++;
-			// 		// echo "The time $time is not within any of the ranges in the \$usage_hours array.\n";
-			// 	}
-			// 	$compareBusLines = $data->bus_line;
-			// 	if($bus_lines == $compareBusLines) {
-			// 		// echo "The Bus lines is same! \n";
-			// 	} else {
-			// 		// Z Variable
-			// 		// echo "The Bus lines not contains! \n";
-			// 		$z++;
-			// 	}
-			// 	}
-			// }
-
-			// dd($x,$y,$z);
-			// dd($output);
 
 			$data = [
 				'card_id' => $card->id,
@@ -411,8 +343,10 @@ class AnalyticsController extends Controller {
 				'non_working_hours' => $y,
 				'non_bus_lines' => $z,
 				'file_url' => $file_url,
+				'nwd_content' => json_encode($non_working_days),
+				'nwh_content' => json_encode($non_working_hours),
+				'nbl_content' =>  json_encode( $non_bus_lines),
 			];
-			// dd($data);
 
 			Analytics::create($data);
 
@@ -431,106 +365,9 @@ class AnalyticsController extends Controller {
 	public function exportAnalyticsAll($id) {
 		$analytics = Analytics::find($id);
 
-		$file_url = $analytics->file_url;
-
-		$command = "python ".public_path('upload/pdfs/scrappingData.py')." ".$file_url;
-
-		exec($command, $output, $return_var);
-
-		$validJsonString = str_replace("'", '"', $output[0]);
-		$outputArray = json_decode($validJsonString);
-
-		$real_hours = array(
-			"1" => "09:00-10:00",
-			"2" => "10:00-11:00",
-			"3" => "11:00-12:00",
-			"4" => "12:00-13:00",
-			"5" => "13:00-14:00",
-			"6" => "14:00-15:00",
-			"7" => "15:00-16:00",
-			"8" => "16:00-17:00",
-			"9" => "17:00-18:00",
-			"10" => "18:00-19:00",
-			"11" => "19:00-20:00",
-			"12" => "20:00-21:00",
-			"13" => "21:00-22:00",
-		);
-
-		$card = Card::where('number',$outputArray->number)->first();
-
-		$x=0; 
-		$y=0; 
-		$z=0; 
-
-		$non_working_days = [];
-		$non_working_hours = [];
-		$non_bus_lines = [];
-		
-		$working_days =  explode(",", $card->working_days);
-		$hours = explode(",", $card->usage_hours);
-		$bus_lines = $card->bus_lines;
-
-		$usage_hours = [];
-
-		foreach ($outputArray->datas as $data){
-
-			if (in_array($data->working_day, $working_days)) {
-				// echo "The date " . $data->working_day . " is present in the array.\n". PHP_EOL;
-			} else {
-				$x++;
-				$non_working_days [] = $data;
-
-				// X Variable
-				// echo "The date " . $data->working_day . " is not present in the array.\n". PHP_EOL;      
-			}
-		
-		
-			foreach ($hours as $key => $value) {
-				if (array_key_exists($value, $real_hours)) {
-					$usage_hours [] = $real_hours[$value];
-				}
-			}
-			
-			// $time = "10:54:31";
-			$time = $data->working_hour;
-			
-			$is_time_found = false;
-			foreach ($usage_hours as $hour_range) {
-				$start_time = explode("-", $hour_range)[0];
-				$end_time = explode("-", $hour_range)[1];
-			
-				$start_time_parts = explode(":", $start_time);
-				$end_time_parts = explode(":", $end_time);
-			
-				$start_time_seconds = ($start_time_parts[0] * 3600) + ($start_time_parts[1] * 60);
-				$end_time_seconds = ($end_time_parts[0] * 3600) + ($end_time_parts[1] * 60);
-			
-				$time_parts = explode(":", $time);
-				$time_seconds = ($time_parts[0] * 3600) + ($time_parts[1] * 60) + $time_parts[2];
-			
-				if ($time_seconds >= $start_time_seconds && $time_seconds <= $end_time_seconds) {
-					$is_time_found = true;
-					// echo "The time $time is within the range " . $hour_range . ".\n";
-					break;
-				}
-			}
-			
-			if (!$is_time_found) {
-				// Y Variable
-				$y++;
-				$non_working_hours [] = $data;
-				// echo "The time $time is not within any of the ranges in the \$usage_hours array.\n";
-			}
-			$compareBusLines = $data->bus_line;
-			if($bus_lines == $compareBusLines) {
-				// echo "The Bus lines is same! \n";
-			} else {
-				// Z Variable
-				// echo "The Bus lines not contains! \n";
-				$z++;
-				$non_bus_lines [] = $data;
-			}
-		}
+		$non_working_days = json_decode($analytics->nwd_content);
+		$non_working_hours = json_decode($analytics->nwh_content);
+		$non_bus_lines = json_decode($analytics->nbl_content);
 
 		$card_number = $analytics->card->number;
 
